518c4d66ef704214a2c456106e0250bf6042df32
umermunir10 <munirumer61@gmail.com>
2023-09-13 17:28:14 +0500

    Not Final Commit

--- AppSource/Controllers/SubscriptionViewControllerSubscriptionViewController/SubscriptionInPurchaseViewController.swift
+++ AppSource/Controllers/SubscriptionViewControllerSubscriptionViewController/SubscriptionInPurchaseViewController.swift
@@ -16,8 +16,7 @@ class SubscriptionInPurchaseViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print("hello")
     }
     
 
